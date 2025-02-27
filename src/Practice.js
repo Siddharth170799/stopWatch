31aae1fdd621ef5c01a4bfe3aad9bf4c99e821e8
siddharth <boorgusiddharth@gmail.com>
2025-02-27 13:39:41 +0530

    cc

--- src/Practice.js
+++ src/Practice.js
@@ -1,39 +1,6 @@
-// import React, { useRef, useState } from "react";
 
-// const Practice = () => {
-//     const [hours,setHours] = useState({a:0,b:0})
-//   const [seconds, setSeconds] = useState(0);
-//   const timerRef = useRef(null);
-
-//   const startTimer = () => {
-//     timerRef.current = setInterval(() => {
-//       setSeconds((prev) => prev + 1);
-//     }, 1000);
-//   };
-//   console.log(seconds);
-
-//   const resetTimer = () => {
-//     clearInterval(timerRef.current);
-//     setSeconds(0);
-//   };
-
-//   const pauseTimer = ()=>{
-//     clearTimeout(timerRef.current)
-    
-//   }
-//   return (
-//     <>
-//       <div>{hours.a}{hours.b}:{seconds}</div>
-//       <button onClick={startTimer}>Start</button>{" "}
-//       <button onClick={resetTimer}>Reset</button>
-//       <button onClick={pauseTimer}>Pause </button>
-//     </>
-//   );
-// };
-
-// export default Practice;
 import React, { useEffect, useRef, useState } from "react";
- // Import the CSS file
+
 
 const Practice = () => {
   const [minutes, setMinutes] = useState(0);
@@ -50,13 +17,14 @@ const Practice = () => {
 
   const resetTimer = () => {
     clearInterval(timerRef.current);
-    timerRef.current = null; // Reset timer reference
+    timerRef.current = null; 
     setSeconds(0);
+    setMinutes(0)
   };
 
   const pauseTimer = () => {
     clearInterval(timerRef.current);
-    timerRef.current = null; // Stop the timer
+    timerRef.current = null; 
   };
 
   useEffect(()=>{
@@ -64,17 +32,44 @@ if(seconds > 59){
     setSeconds(0)
     setMinutes((prev)=> prev+1)
 }
+
+if(seconds == 0){
+    resetTimer()
+}
   },[seconds])
+
+  const reverseTimer=()=>{
+
+    if(seconds > 0){
+        clearInterval(timerRef.current)
+        timerRef.current = setInterval(()=>{
+        
+                setSeconds((prev)=> {
+                    if(prev > 0){
+                        return prev - 1
+                    }
+                })
+          
+        },1000)
+    }else{
+        clearInterval(timerRef.current)
+    }
+    
+  }
+
+
+ console.log(seconds)
 
   return (
     <div className="timer-container">
       <div className="timer-display">
-       {seconds < 60 ? `0${minutes}` : minutes   }: {seconds < 10 ? `0${seconds}` : seconds}
+       {minutes < 10 ? `0${minutes}` : minutes   } : {seconds < 10 ? `0${seconds}` : seconds}
       </div>
       <div className="button-group">
         <button className="btn start" onClick={startTimer}>Start</button>
         <button className="btn pause" onClick={pauseTimer}>Pause</button>
         <button className="btn reset" onClick={resetTimer}>Reset</button>
+        <buton className="btn reverse" onClick={reverseTimer}>Reverse</buton>
       </div>
     </div>
   );
