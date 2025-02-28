dd1f292647bc483ef6657809e04d38c972d5b9cc
siddharth <boorgusiddharth@gmail.com>
2025-02-28 12:07:56 +0530

    bb

--- src/Practice.js
+++ src/Practice.js
@@ -1,75 +1,113 @@
-
 import React, { useEffect, useRef, useState } from "react";
-
 
 const Practice = () => {
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
   const timerRef = useRef(null);
+  const [displayStart, setDisplayStart] = useState(true);
+  const [displayPause, setDisplayPause] = useState(false);
+  const [displayReset, setDisplayReset] = useState(false);
+  const [displayReverse, setDisplayReverse] = useState(false);
 
   const startTimer = () => {
-    if (!timerRef.current) {
-      timerRef.current = setInterval(() => {
-        setSeconds((prev) => prev + 1);
-      }, 1000);
-    }
+    timerRef.current = setInterval(() => {
+      setSeconds((prev) => prev + 1);
+    }, 1000);
+    setDisplayPause(true);
+    setDisplayReset(true);
+    setDisplayReverse(true);
+    setDisplayStart(false);
   };
 
   const resetTimer = () => {
     clearInterval(timerRef.current);
-    timerRef.current = null; 
+    timerRef.current = null;
     setSeconds(0);
-    setMinutes(0)
+    setMinutes(0);
+    setDisplayStart(true);
+    setDisplayReset(false);
+    setDisplayReverse(false);
+    setDisplayPause(false);
   };
 
   const pauseTimer = () => {
     clearInterval(timerRef.current);
-    timerRef.current = null; 
+    timerRef.current = null;
+    setDisplayPause(false);
+    setDisplayReset(true);
+    setDisplayReverse(true);
+    setDisplayStart(true);
   };
 
-  useEffect(()=>{
-if(seconds > 59){
-    setSeconds(0)
-    setMinutes((prev)=> prev+1)
-}
-
-if(seconds == 0){
-    resetTimer()
-}
-  },[seconds])
-
-  const reverseTimer=()=>{
-
-    if(seconds > 0){
-        clearInterval(timerRef.current)
-        timerRef.current = setInterval(()=>{
-        
-                setSeconds((prev)=> {
-                    if(prev > 0){
-                        return prev - 1
-                    }
-                })
-          
-        },1000)
-    }else{
-        clearInterval(timerRef.current)
+  useEffect(() => {
+    if (seconds > 59) {
+      setSeconds(0);
+      setMinutes((prev) => prev + 1);
     }
-    
-  }
+    if (seconds == 0) {
+      reverseTimer();
+    }
+  }, [seconds]);
 
+  const reverseTimer = () => {
+    if (displayReverse) {
+      setDisplayStart(false);
+    }
+    if (minutes > 0) {
+      if (seconds == 0) {
+        setSeconds(59);
+        setMinutes((minutes) => minutes - 1);
+      } else {
+        clearInterval(timerRef.current);
+        timerRef.current = setInterval(() => {
+          setSeconds((seconds) => seconds - 1);
+        }, 1000);
+      }
+    } else {
+      clearInterval(timerRef.current);
+      timerRef.current = setInterval(() => {
+        if (seconds > 0) {
+          setSeconds((seconds) => seconds - 1);
+        }
+      }, 1000);
+      if (seconds == 0) {
+        clearInterval(timerRef.current);
 
- console.log(seconds)
+        setDisplayPause(false);
+        setDisplayReset(false);
+        setDisplayReverse(false);
+        setDisplayStart(true);
+      }
+    }
+  };
 
   return (
     <div className="timer-container">
       <div className="timer-display">
-       {minutes < 10 ? `0${minutes}` : minutes   } : {seconds < 10 ? `0${seconds}` : seconds}
+        {minutes < 10 ? `0${minutes}` : minutes} :{" "}
+        {seconds < 10 ? `0${seconds}` : seconds}
       </div>
       <div className="button-group">
-        <button className="btn start" onClick={startTimer}>Start</button>
-        <button className="btn pause" onClick={pauseTimer}>Pause</button>
-        <button className="btn reset" onClick={resetTimer}>Reset</button>
-        <buton className="btn reverse" onClick={reverseTimer}>Reverse</buton>
+        {displayStart && (
+          <button className="btn start" onClick={startTimer}>
+            Start
+          </button>
+        )}
+        {displayPause && (
+          <button className="btn pause" onClick={pauseTimer}>
+            Pause
+          </button>
+        )}
+        {displayReset && (
+          <button className="btn reset" onClick={resetTimer}>
+            Reset
+          </button>
+        )}
+        {displayReverse && (
+          <button className="btn reverse" onClick={reverseTimer}>
+            Reverse
+          </button>
+        )}
       </div>
     </div>
   );
